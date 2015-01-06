def3b31a017e363e4bb2bbf0ed7d4cd9dc7d44b9
aandinvizzble <raf.flamand@hotmail.com>
2015-01-06 21:49:19 +0100

    litlle fault ;)

--- java/com/invizzble/SC/SC.java
+++ java/com/invizzble/SC/SC.java
@@ -38,6 +38,7 @@ public class SC {
 	 * 
 	 * -Device that creates wormholes to teleport you through space and time (time is just changing the minecraft time)
 	 * 
+	 * 
 	 * TODO: 
 	 * -Balance the power system
 	 * 
