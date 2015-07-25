0367b9aa00e916f7822e5ad2d382196ffe1a4a50
Invizzble <waz.zap@hotmail.com>
2015-07-25 02:28:11 +0200

    Scipad update (again)
    Added a good amount of scipad gui content

--- java/com/invizzble/SC/client/interfaces/GuiSciPad.java
+++ java/com/invizzble/SC/client/interfaces/GuiSciPad.java
@@ -4,10 +4,13 @@ import java.awt.Color;
 import java.awt.List;
 import java.util.ArrayList;
 
+import javax.vecmath.Vector2f;
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.texture.TextureMap;
+import net.minecraft.client.shader.TesselatorVertexState;
 import net.minecraft.util.IIcon;
 import net.minecraft.util.ResourceLocation;
 
@@ -17,12 +20,14 @@ import com.invizzble.SC.lib.Info;
 import com.invizzble.SC.research.ModResearch;
 import com.invizzble.SC.research.PageResearch;
 import com.invizzble.SC.research.Research;
+import com.invizzble.SC.research.ResearchTest;
 
 import cpw.mods.fml.client.config.HoverChecker;
 
 public class GuiSciPad extends GuiScreen{
     /**
      * This class is really a mess... It just won't work the way I want it so i'll brute force it a bit.
+     * 			-> can be lots better, will be when i redo the mod, but it won't be directly :p (there first has to be a mod to be redone
      */
 	private static final ResourceLocation texture = new ResourceLocation(Info.MOD_ID+":"+"textures/gui/SciPad.png");
 	
@@ -53,6 +58,8 @@ public class GuiSciPad extends GuiScreen{
 	
 	int tempMouseX;
 	int tempMouseY;
+	
+	double animateLine = 0;
 	
 	boolean isClicked = false;		
 	
@@ -143,7 +150,7 @@ public class GuiSciPad extends GuiScreen{
 				GL11.glColor4f(0.5F, 0.5F, 0.5F, 1.0F);
 			}
 			Minecraft.getMinecraft().getTextureManager().bindTexture(texture);
-			if((x > this.x && x < this.x + scWidth) && (y > this.y && y < this.y+scHeight)){
+			if(((x+researchSide/2) >= this.x + 5 && (x-researchSide/2) <= this.x + scWidth) && ((y+researchSide/2) >= this.y + 5 && (y-researchSide/2) <= this.y+scHeight - 10)){
 				drawTexturedModalRect(x-(researchSide/2), y-(researchSide/2), 0, 256-researchSide, researchSide, researchSide);
 				Minecraft.getMinecraft().getTextureManager().bindTexture(TextureMap.locationItemsTexture);
 				drawTexturedModelRectFromIcon(x-(researchIconSide/2), y-(researchIconSide/2), icon, researchIconSide, researchIconSide);
@@ -193,62 +200,89 @@ public class GuiSciPad extends GuiScreen{
 			for(Research required:research.getRequiredResearch()){
 				int otherX = this.x + scWidth/2 - (int)((focusX - 64 -required.getPageX() - 4) * pageScaleWidth);
 				int otherY = this.y + scHeight/2 - (int)((focusY - 32 - required.getPageY() - 4)* pageScaleHeight);
-				if(otherX < this.x){
-					otherX = this.x;
+				int width;
+				int height;
+				
+				if(x > otherX){
+					width = research.getPageX() - required.getPageX();
+				}else{
+					width = required.getPageX() - research.getPageX();
 				}
+				if(y > otherY){
+					height = research.getPageY() - required.getPageY();
+				}else{
+					height = required.getPageY() - research.getPageY();
+				}
+				
+				//Using thales to keep the line straight if it's past the borders! adjustement = (overflow/(width or height)*(width or height)
 				if(x < this.x){
+					try{
+						y  += (int)((float)(Math.abs(x - this.x ))/width * height);
+					}catch(ArithmeticException e){
+						
+					}
 					x = this.x;
 				}
 				
 				if(x > this.x + scWidth){
+					try{
+						y  -= (int)((float)(Math.abs(x -(this.x + scWidth)))/width * height);
+					}catch(ArithmeticException e){
+						
+					}
 					x = this.x+scWidth;
 				}
 				
 				if(otherX > this.x + scWidth){
+					try{
+						otherY  -= (int)(float)(Math.abs(otherX-(this.x + scWidth))/width * height);
+					}catch(ArithmeticException e){
+						
+					}
 					otherX = this.x+scWidth;
 				}
 				
 				if(otherX < this.x){
+					try{
+					otherY  += (int)((float)(Math.abs(otherX - this.x))/width * height);				
+					}catch(ArithmeticException e){
+						
+					}
 					otherX = this.x;
 				}
 				
 				if(otherY < this.y){
+					try{
+						otherX  += (int)((float)(Math.abs(otherY - this.y))/ height * width);
+					}catch(ArithmeticException e){
+						
+					}
 					otherY = this.y;
 				}
 				
 				if(y < this.y){
+					try{
+						x  += (int)((float)(Math.abs(y - this.y))/ height * width);
+					}catch(ArithmeticException e){
+						
+					}
 					y = this.y;
 				}
 				
 				if(otherY > this.y + scHeight){
+					
 					otherY = this.y+scHeight;
 				}
 				
 				if(y > this.y+scHeight){
+					x  -= (int)((float)(Math.abs(y - (this.y + scHeight)))/ height * width);
 					y = this.y+scHeight;
 				}
 				
-				if(otherY > y){
-					y+=researchSide/2;
-					otherY-=researchSide/2;
-				}else{
-					otherY+=researchSide/2;
-					y-=researchSide/2;
-				}
 				//color = (alpha)(red)(green)(blue)
 				int color = 0xFFD9D9D9;
-				if((otherY > this.y && y < this.y + scHeight)||(otherY < this.y + scHeight && y > this.y)){
-					drawHorizontalLine(x,otherX, y - 1, color);
-					drawHorizontalLine(x,otherX, y, color);
-					drawHorizontalLine(x,otherX, y + 1, color);
-				}
-				if((otherX > this.x && x < this.x + scWidth)||(otherX < this.x + scWidth && x > this.x)){
-					drawVerticalLine(otherX - 1, y, otherY, color);
-					drawVerticalLine(otherX, y, otherY, color);
-					drawVerticalLine(otherX + 1, y, otherY, color);
-				}
 				
-				//drawLine(x, y, otherX, otherY);
+				drawLine(x, y, otherX, otherY, color);
 			}
 		}
 		GL11.glPopMatrix();
@@ -256,16 +290,52 @@ public class GuiSciPad extends GuiScreen{
 	
 	
 	//problem with coloring
-	public void drawLine(int xStart, int yStart, int xEnd, int yEnd){
+	public void drawLine(int xStart, int yStart, int xEnd, int yEnd, int color){
+		double width ;
+		double height;
+		if(xStart > xEnd){
+			width = -(xStart - xEnd);
+		}else{
+			width = xEnd - xStart;
+		}
+		if(yStart > yEnd){
+			height = -(yStart - yEnd);
+		}else{
+			height = yEnd - yStart;
+		}
+		double length = Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2));
+		if(xEnd <= xStart){
+			length = -length;
+		}
+		GL11.glTranslatef((float)xStart, (float)yStart, 0);
+		if(width != 0){
+			GL11.glRotated(Math.toDegrees(Math.atan2(height, width)), 0.0F, 0.0F, 1.0F);
+		}else{
+			if(yStart < yEnd){
+				GL11.glRotated(90, 0.0F, 0.0F, 1.0F);
+			}else{
+				GL11.glRotated(-90 , 0.0F, 0.0F, 1.0F);
+			}
+		}
+		GL11.glEnable(GL11.GL_LINE_SMOOTH);
+		GL11.glLineWidth(2.0F);
 		Tessellator tes = Tessellator.instance;
 		tes.startDrawing(GL11.GL_LINES);
-		GL11.glDisable(GL11.GL_LIGHTING);
-		tes.setColorRGBA_F(1.0F, 1.0F, 1.0F, 1.0F);
-		tes.addVertex(xStart - 1, yStart, 1);
-		tes.addVertex(xStart, yStart, 1);
-		tes.addVertex(xStart + 1, yStart, 1);
-		tes.addVertex(xEnd, yEnd, 1);
+		GL11.glDisable(GL11.GL_TEXTURE_2D);
+		tes.setColorOpaque_I(color);
+		for(int i = 0; i < Math.abs(length); i+=1){
+			tes.addVertex(i, 5*Math.sin(0.1*i + animateLine) , 0);
+			tes.addVertex(i + 1, 5*Math.sin(0.1*(i - 1) + animateLine), 0);
+
+		}
+		if(animateLine < 31.5){
+			animateLine+=0.1;
+		}else{
+			animateLine = 0;
+		}
 		tes.draw();
+		GL11.glEnable(GL11.GL_TEXTURE_2D);
+		GL11.glDisable(GL11.GL_LINE_SMOOTH);
 	}
 	
 
